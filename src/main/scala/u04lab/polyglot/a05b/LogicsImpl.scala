f45843f291f795a9d73bb6ce69d4c2fb8f2f084e
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-18 10:48:47 +0100

    to scala 3 style

--- src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
@@ -1,9 +1,8 @@
 package u04lab.polyglot.a05b
 
-class LogicsImpl(size: Int) extends Logics {
+class LogicsImpl(size: Int) extends Logics:
   override def tick(): Unit = {}
 
   override def isOver: Boolean = false
 
   override def hasElement(x: Int, y: Int): Boolean = false
-}
