875df52b6cb0c906db4a5ba29f1c9f620ecd53d6
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-18 12:49:06 +0100

    minor on a01b

--- src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01b/LogicsImpl.scala
@@ -4,6 +4,6 @@ import scala.jdk.javaapi.OptionConverters
 /** solution at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01b/sol2/ */
 class LogicsImpl(size: Int, mines: Int) extends Logics:
   def hit(x: Int, y: Int): java.util.Optional[Integer] =
-    OptionConverters.toJava(None)
+    OptionConverters.toJava(None) // Option => Optional converter
 
   def won: Boolean = false
