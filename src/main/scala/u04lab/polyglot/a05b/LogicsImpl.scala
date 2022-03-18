0d5bec896805d7f200c12ecff805b54fdf909443
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-18 11:29:14 +0100

    add solution reference

--- src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a05b/LogicsImpl.scala
@@ -1,5 +1,6 @@
 package u04lab.polyglot.a05b
 
+/** solution at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(size: Int) extends Logics:
   override def tick(): Unit = {}
 
