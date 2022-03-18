c1e22ebde5b083ffebe4de607e943f71ef4c1eea
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-18 12:57:50 +0100

    fix comments

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,6 +1,6 @@
 package u04lab.polyglot.a01a
 import Logics.*
 
-/** solution at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
+/** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(size: Int, boat: Int) extends Logics:
   def hit(row: Int, col: Int): Result = Result.HIT
